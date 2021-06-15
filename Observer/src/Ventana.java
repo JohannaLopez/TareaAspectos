46dac9b6ee61ca7bdb91d0c2e591f04bb2eb6ed4
ycedenog <yonmaced@espol.edu.ec>
2021-06-14 21:45:23 -0500

    Se añadio correctamente la nueva funcionalidad a la ventana principal

--- Observer/src/Ventana.java
+++ Observer/src/Ventana.java
@@ -8,6 +8,7 @@ public class Ventana extends JFrame {
 	private JButton boton1; 
 	private JButton boton2;
 	private JButton boton3;
+	private JButton boton4;
 	ArrayList<String> nombreLibros = new ArrayList<String>();
 	JTextField campo = new JTextField(20);
 
@@ -27,7 +28,7 @@ public class Ventana extends JFrame {
 	
 	private void configurarVentana() {
 		 this.setTitle("Tarea Aspectos");                   // colocamos titulo a la ventana
-		 this.setSize(310, 210);                                 // colocamos tamanio a la ventana (ancho, alto)
+		 this.setSize(700, 800);                                 // colocamos tamanio a la ventana (ancho, alto)
 	     this.setLocationRelativeTo(null);                       // centramos la ventana en la pantalla
 	     this.setLayout(null);                                   // no usamos ningun layout, solo asi podremos dar posiciones a los componentes
 	     this.setResizable(false);                               // hacemos que la ventana no sea redimiensionable
@@ -35,15 +36,22 @@ public class Ventana extends JFrame {
 	}
 
 	private void inicializarComponentes() {
+		aniadirElementos();
+		mostrarLibros();
 		boton1 = new JButton();	
 		boton2 = new JButton();
 		boton3 = new JButton();
+		boton4 = new JButton();
 		boton1.setText(getColourName(colors[0]));   // colocamos un texto al boton		
 		boton1.setBounds(50, 20, 200, 30);  // colocamos posicion y tamanio al boton (x, y, ancho, alto)		 
 		boton2.setText(getColourName(colors[1]));   // colocamos un texto al boton
 		boton2.setBounds(50, 60, 200, 30);  // colocamos posicion y tamanio al boton (x, y, ancho, alto)		  
 		boton3.setText(getColourName(colors[2]));   // colocamos un texto al boton
 		boton3.setBounds(50, 100, 200, 30);  // colocamos posicion y tamanio al boton (x, y, ancho, alto)
+		boton4.setBounds(300, 140, 200, 30);// colocamos la posicion y tamanio al boton
+		boton4.setText("Comprar");
+		campo.setBounds(50,140,200,30);
+		
 		boton1.addActionListener((new ActionListener(){  
 		public void actionPerformed(ActionEvent e){  
 			accionBoton1(e,colors[0]);
@@ -59,11 +67,25 @@ public class Ventana extends JFrame {
 				accionBoton3(e,colors[2]);
 		}  
 		}));
-		LaminaTexto miLamina = new LaminaTexto();
-		this.add(miLamina);
+		
+		boton4.addActionListener((new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String ingreso = campo.getText().toLowerCase();
+				nombreLibros.removeIf(nombre -> ((nombre.toLowerCase()).equals(ingreso)));
+				mostrarLibros();
+				
+			}
+		}));
+		
+		
+		
 		this.add(boton1);
 		this.add(boton2);
 		this.add(boton3);
+		this.add(campo);
+		this.add(boton4);
 	}
 	
 	private void accionBoton1(ActionEvent e, Color color){
@@ -91,33 +113,10 @@ public class Ventana extends JFrame {
 	    return name;
 	}
 
-	public class LaminaTexto extends JPanel{
+	
 
-		public LaminaTexto(){
-			aniadirElementos();
-			mostrarLibros();
-
-			add(campo);
-			JButton boton1 = new JButton("Comprar");
-
-			Texto evento = new Texto();
-			boton1.addActionListener(evento);
-
-			add(boton1);
-		}
-
-	// Clase interna que obtiene el texto que escribe el usuario
-	private class Texto implements ActionListener{
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			String ingreso = campo.getText().toLowerCase();
-			nombreLibros.removeIf(nombre -> ((nombre.toLowerCase()).equals(ingreso)));
-			mostrarLibros();
-		}
-
-	}
-	//Metodo que añade libros al arraylist
+	
+	//Metodo que aniade libros al arraylist
 	public void aniadirElementos() {
 
 		nombreLibros.add("El principito");
@@ -134,4 +133,3 @@ public class Ventana extends JFrame {
 			}
 		}
 	}
-}
